08b5d163d19963030207d594e9ac9bc9546815fc
Bice93 <bianca.cerulo@hotmail.it>
2022-07-20 14:40:38 +0200

    Added GenderSearchBar

--- src/components/DiscList.vue
+++ src/components/DiscList.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="container msCardsBox">
-        <div class="containerCards">
-            <DiscCard
-                v-for="(element, index) in discsList"
-                :key="index"
-                :discElement="element"
-            />
-        </div>
+    <div class="boxSelect_gender">
+      <GenderSearchBar/>
+    </div>
+
+    <div class="containerCards">
+      <DiscCard
+        v-for="(element, index) in discsList"
+        :key="index"
+        :discElement="element"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import DiscCard from "./DiscCard.vue";
+import GenderSearchBar from "./GenderSearchBar.vue";
 
 export default {
   components: {
     DiscCard,
+    GenderSearchBar,
   },
 
   data: function () {
@@ -32,7 +38,7 @@ export default {
       axios
         .get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((result) => {
-          // console.log(result.data.response)
+          //console.log(result.data.response)
           this.discsList = result.data.response;
           console.log(this.discsList);
         })
@@ -49,6 +55,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.boxSelect_gender{
+  margin: 30px 0;
+}
+
 .containerCards {
   display: flex;
   flex-wrap: wrap;
---
+++ src/components/GenderSearchBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+     <label> Scegli il genere: </label>
+      <select>
+        <option value="0">Rock</option>
+        <option value="1">Pop</option>
+        <option value="2">Jazz</option>
+        <option value="3">Metal</option>
+      </select>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.boxSelect_gender select{
+    padding: 5px;
+    background-color: #2e3a46;
+    border: 1px solid #2e3a46;
+    color: white;
+    width: 150px;
+    height: 25px;
+}
+</style>
